69bd2588bb91364353c566a710590b8400cfc490
SimoneFerrini <ferrini.simone4@gmail.com>
2023-05-09 15:30:25 +0200

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+<header>
+    <!--barra azzurra-->
+    <div class="bg-azzurro">
+        <div class="container">
+            <span>DC Power Visa <i class="fa-regular fa-registered"></i></span>
+            <span>Additional DC Sites <i class="fa-solid fa-caret-down"></i></span>
+        </div>
+    </div>
+    <!--navbar -->
+
+    <!-- jumbotron -->
+
+</header>
