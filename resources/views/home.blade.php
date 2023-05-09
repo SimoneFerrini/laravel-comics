8518ebd5e8c88496aa5f2d9af0272f40cad160ea
SimoneFerrini <ferrini.simone4@gmail.com>
2023-05-09 18:52:14 +0200

    started main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts/main-layout')
 
 @section('content')
-    
+    <main>
+        <!--main top black-->
+        <div class="main-bg-top">
+            <div class="container">
+                <span class="current-series">current series</span>
+               
+                <div class="main-cards-container">
+
+                </div>
+
+                <div>
+                    <button class="load-more-btn">load more</button>
+                </div>
+
+            </div>
+        </div>
+        <!-- main bot dogerblue-->
+        <div class="main-bg-bot">
+            <div class="container">
+
+            </div>
+        </div>
+    </main>
 @endsection
