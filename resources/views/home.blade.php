503760ab6321e79676365148383dced2a67b9369
SimoneFerrini <ferrini.simone4@gmail.com>
2023-05-09 19:25:18 +0200

    ended without bonus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,14 @@
                 <span class="current-series">current series</span>
                
                 <div class="main-cards-container">
-
+                    @foreach($comics as $singleComic)
+                    <div class="my-card">
+                        <div class="card-img-container">
+                            <img src="{{$singleComic['thumb']}}" alt="comicImage">
+                        </div>
+                        <span>{{$singleComic['title']}}</span>
+                    </div>
+                    @endforeach
                 </div>
 
                 <div>
@@ -20,7 +27,14 @@
         <!-- main bot dogerblue-->
         <div class="main-bg-bot">
             <div class="container">
-
+                @foreach ($mainLinks as $mainlink)
+                    <div class="img-text-container">
+                        <div class="main-bot-img-container">
+                            <img src="{{Vite::asset($mainlink['img'])}}" alt="img">
+                        </div>
+                        <span>{{$mainlink['text']}}</span>
+                    </div>
+                @endforeach
             </div>
         </div>
     </main>
--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,7 @@ Route::get('/', function () {
     ];
 
     $comics = config('comics');
+    $mainLinks = config('mainbottomlinks');
 
-    return view('home', compact('links', 'comics'));
+    return view('home', compact('links', 'comics', 'mainLinks'));
 });
